d45f61e20910d8942394d24f510039a0ddcfd667
R <rizkyramadhanpd@gmail.com>
2025-03-14 20:48:07 +0700

    feat: add theme button

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { ThemeProvider } from "./context/ThemeProvider";
+import { ThemeProvider } from "@/context/ThemeProvider";
 import App from "@/App.jsx";
 import "@/globals.css";
 
