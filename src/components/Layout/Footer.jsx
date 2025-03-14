e6ad33a5365cce1d6d8e6cdaab41c34067d364e3
R <rizkyramadhanpd@gmail.com>
2025-03-14 21:08:20 +0700

    style: customize the footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,13 +4,13 @@ export function Footer() {
   return (
     <footer
       aria-labelledby="website-footer"
-      className="border-t border-zinc-300 py-8 text-center text-sm text-gray-600 sm:text-left"
+      className="border-t border-gray-300 dark:border-gray-700 py-6 text-center text-sm text-gray-600 dark:text-gray-300 sm:text-left"
     >
       <span id="website-footer" className="sr-only">
         Website footer
       </span>
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <p className="block sm:inline text-shadow-2">
+        <p className="block sm:inline">
           &copy; {currentYear} Ryzmdn. All rights reserved.
         </p>
       </div>
