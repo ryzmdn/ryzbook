534e1b133a0aaaed50ea0a7877732ea85ae8aea3
R <rizkyramadhanpd@gmail.com>
2025-03-14 22:35:13 +0700

    style: add disabled text and background colors

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 import { clss } from "@/utils/clss";
 
 const variantStyles = {
-  primary: "bg-pink-600 text-gray-50 hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600",
+  primary: "bg-pink-600 text-gray-50 hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 disabled:text-gray-500 disabled:bg-gray-400 dark:disabled:bg-gray-800",
   secondary: "text-gray-950 dark:text-gray-50 bg-gray-50 dark:bg-gray-950 hover:bg-gray-100 dark:hover:bg-gray-900 ring-1 ring-inset ring-gray-300 dark:ring-gray-700",
   soft: "bg-gray-50/10 hover:bg-gray-50/20 text-gray-950 dark:text-gray-50",
   text: "bg-transparent text-gray-950 dark:text-gray-50",
